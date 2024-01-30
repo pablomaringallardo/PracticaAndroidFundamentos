d63ffd8f6f7834a46bf7148732d6a2bf0ec8f656
Pablo Marín <pablomaringallardo17@gmail.com>
2024-01-30 12:42:19 +0100

    Práctica fundamentos de Android Pablo

---
+++ app/src/main/java/com/example/practicaandroidfundamentos/Heroe.kt
@@ -0,0 +1,13 @@
+package com.example.practicaandroidfundamentos
+
+
+data class Heroe(
+    val photo: String,
+    val id: String,
+    val favorite: Boolean,
+    val description: String,
+    val name: String,
+    var totalHitPoints : Int = 100,
+    var currentHitPoints : Int = 100,
+    var isDead : Boolean = false
+)
---
+++ app/src/main/java/com/example/practicaandroidfundamentos/HeroesListFragment.kt
@@ -0,0 +1,64 @@
+package com.example.practicaandroidfundamentos
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.practicaandroidfundamentos.databinding.FragmentHeroesListBinding
+import kotlinx.coroutines.launch
+
+class HeroesListFragment : Fragment(),onClickGridItem {
+
+    private lateinit var binding: FragmentHeroesListBinding
+    val coreViewModel: CoreViewModel by activityViewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHeroesListBinding.inflate(inflater)
+        setFloatingHealAllHeroesButton()
+
+        val adapter = FragmentListAdapter(coreViewModel.heroesList,this)
+        binding.rvHeroesList.layoutManager = GridLayoutManager(binding.rvHeroesList.context,2)
+        binding.rvHeroesList.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch{
+            coreViewModel.uiState.collect{
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onClick(heroe: Heroe) {
+        coreViewModel.selectedHeroToFightClicked(heroe)
+    }
+
+    private fun setFloatingHealAllHeroesButton(){
+        binding.buttonHealtHeroe.setOnClickListener {
+            coreViewModel.healAllHeroes()
+            Toast.makeText(binding.buttonHealtHeroe.context,"Todos los heroes han sido curados", Toast.LENGTH_LONG).show()
+
+
+        }
+    }
+
+
+
+}
